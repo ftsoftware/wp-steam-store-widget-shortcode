d57e2eaa8910025e1059e493d2762667355c4ebf
Gino Messmer <g.messmer@ftsoftware.eu>
2015-09-13 18:14:54 +0200

    Standard Header Section

---
+++ steam-store-shortcode/steam-store-shortcode.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Plugin Name: Steam Store Widget Shortcode
+* Plugin URI: http://ftsoftware.github.io/wp-steam-store-shortcode
+* Description: A simple shortcode which displays a Steam store widget within posts in WordPress.
+* Version: 1.0
+* Author: Gino Messmer
+* Author URI: http://github.com/ginomessmer
+* License: GPL 3.0
+*/
+
+?>
