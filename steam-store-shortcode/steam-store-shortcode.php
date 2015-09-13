cf3528d87bca65351410e81b3488d38e216e6c06
Gino Messmer <g.messmer@ftsoftware.eu>
2015-09-13 18:44:07 +0200

    Steam Store Page Widget

--- steam-store-shortcode/steam-store-shortcode.php
+++ steam-store-shortcode/steam-store-shortcode.php
@@ -6,7 +6,19 @@
 * Version: 1.0
 * Author: Gino Messmer
 * Author URI: http://github.com/ginomessmer
-* License: GPL 3.0
+* License: GPL 3.0. See license file for more information.
 */
 
+	// Steam Store Widget Shortcode Function
+	function store_widget_builder($atts) {
+		extract(shortcode_atts(array(
+			'id' => 0,
+			'width' => '100%',
+		), $atts));
+
+		$html_widget = '<iframe src="https://store.steampowered.com/widget/' . $id . '/" width="' . $width .  '" height="190" frameBorder="0"></iframe>';
+		return $html_widget;
+	}
+	
+	add_shortcode('steam-store-widget', 'store_widget_builder');					// Steam Store
 ?>
